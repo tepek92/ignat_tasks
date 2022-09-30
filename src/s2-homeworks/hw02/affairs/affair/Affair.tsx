1994e5b5de3f636e45e092c06ecf72cfc08b40a4
tepek92 <tepek92@mail.ru>
2022-09-30 16:44:19 +0700

    hw2 done

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -6,43 +6,26 @@ import s2 from '../Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
-    }
+    const {affair, deleteAffairCallback} = {...props}
+    const deleteCallback = (_id: number) => deleteAffairCallback(_id);
 
-    const nameClass = s.name + ' ' + s2[props.affair.priority]
-    const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
-    const affairClass = s.affair + ' ' + s2[props.affair.priority]
+    const nameClass = s.name + ' ' + s2[affair.priority]
+    const buttonClass = s.closeButton + ' ' + s2[affair.priority]
+    const affairClass = s.affair + ' ' + s2[affair.priority]
 
     return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
-            <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
-            <div id={'hw2-priority-' + props.affair._id} hidden>
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
-
+        <div id={'hw2-affair-' + affair._id} className={affairClass}>
+            <div id={'hw2-name-' + affair._id} className={nameClass}>{affair.name}</div>
+            <div id={'hw2-priority-' + affair._id} hidden>{affair.priority}</div>
             <button
-                id={'hw2-button-delete-' + props.affair._id}
+                id={'hw2-button-delete-' + affair._id}
                 className={buttonClass}
-                // need to fix
-
-            >
-                {/*текст кнопки могут изменить студенты*/}
+                onClick={() => deleteCallback(affair._id)}>
                 X
-                {/**/}
             </button>
         </div>
     )
