39821ffacd1ee478972cd388f7082bb400017ead
tepek92 <tepek92@mail.ru>
2022-10-11 13:08:38 +0700

    hw5 is done

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -6,9 +6,9 @@ import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
+    PRE_JUNIOR: '/pre_junior',
     JUNIOR: '/junior',
-    JUNIOR_PLUS: '/testlink',
+    JUNIOR_PLUS: '/junior_plus',
 }
 
 function Pages() {
