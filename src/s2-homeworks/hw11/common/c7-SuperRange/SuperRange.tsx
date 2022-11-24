44c280232898262672d2c1efaa53225dca3662df
tepek92 <tepek92@mail.ru>
2022-11-24 13:39:30 +0700

    hw11 fixed poit control

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,14 +5,30 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                margin: '0 20px',
+                margin: '0 22px 0 0',
                 color: '#00CC22',
                 height: 4,
                 width: 150,
                 '& .MuiSlider-thumb': {
-                    backgroundColor: 'white',
-                    border: '2px solid'
-                },
+                    // 0 variant
+                    // height: 18,
+                    // width: 18,
+                    // backgroundColor: 'white',
+                    // border: '1px solid #00CC22'
+
+                    // 1 variant
+                    // height: 10,
+                    // width: 10,
+                    // backgroundColor: '#00CC22',
+                    // boxShadow: '0 0 0 5px #fff, 0 0 0 7px #00CC22',
+
+                    // 2 variant
+                    height: 18,
+                    width: 18,
+                    border: "solid 5px white",
+                    outline: "solid 2px #00CC22",
+
+            },
                 '& .MuiSlider-rail': {
                     backgroundColor: 'black',
                 }
